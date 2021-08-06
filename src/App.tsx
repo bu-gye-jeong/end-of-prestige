0d392d5c75a6eb510bd2f5c30d8e0dd8915fa667
bu-gye-jeong <jaey20179@gmail.com>
2021-08-06 20:33:35 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Decimal from "decimal.js";
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="topBar" />
+    </div>
+  );
+}
+
+export default App;
