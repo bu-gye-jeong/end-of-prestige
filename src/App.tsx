f944931bec1d78a5866de408f788104ff926e521
bu-gye-jeong <jaey20179@gmail.com>
2021-08-07 12:12:32 +0900

    added gh-pages andbasic things

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import Decimal from "decimal.js";
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div id="topBar" />
-    </div>
-  );
+interface GameProps {}
+
+class Game extends React.Component<GameProps> {
+  constructor(props: GameProps) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="topBar">Lorem ipsum dolor sit amet, consectetur adipisicing elit </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Game;
